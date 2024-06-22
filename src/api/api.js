10bf50da74b99bd75f23fe5822ba30f19df93569
Parmeet <parmeet@wizcommerce.com>
2024-06-22 05:40:53 +0530

    weather app

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = 'a2bca996c5cdf3fd1ee17783186c593a';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
